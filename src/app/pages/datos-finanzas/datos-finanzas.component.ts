afe76a93cdf17d4aed96850d48f10d43970425ec
Guillermo Tobon <memot18@hotmail.com>
2022-02-26 19:11:35 -0500

    Ajuste en BD

--- src/app/pages/datos-finanzas/datos-finanzas.component.ts
+++ src/app/pages/datos-finanzas/datos-finanzas.component.ts
@@ -4,6 +4,7 @@ import { FinanzasService } from 'src/app/services/finanzas.service';
 import { Router } from '@angular/router';
 import { JsonpInterceptor } from '@angular/common/http';
 import { AuthService } from 'src/app/services/auth.service';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-datos-finanzas',
@@ -19,6 +20,7 @@ export class DatosFinanzasComponent implements OnInit {
   public valIngresos:number = 0;
   public valEgresos:number = 0;
   public totalAcumula:number = 0;
+  public saldoRojo:string = '';
 
   constructor(
               private finanzasServ: FinanzasService,
@@ -40,29 +42,35 @@ export class DatosFinanzasComponent implements OnInit {
    * @param idUs => ID del usuario
    */
   public getFinanzasById = (idUs:any) =>{
-    this.finanza = JSON.parse(localStorage.getItem('finanzas')) || [];
+    this.finanza = JSON.parse(localStorage.getItem('finanzas')).map(  (fina:any) => fina ) || [];
     
     //Obtenemos los ingresos
-    this.finanzasServ.getIngresosByIdService(idUs).subscribe( (resp2:any) =>{
+      this.finanzasServ.getIngresosByIdService(idUs).subscribe( (resp2:any) =>{
       this.ingresos = resp2.ingresos || [];
       this.ingresos.forEach( ingre =>{
         this.valIngresos += ingre.valor_ingre;
+      });
+    }, (err) =>{ console.error(err)});
+
+
+    //Obtenemos los egresos
+    this.finanzasServ.getEgresosByIdService(idUs).subscribe( (resp3:any) =>{
+      this.egresos = resp3.egresos || [];
+      this.egresos.forEach( egre =>{
+        this.valEgresos += egre.valor_egre;
       })
-
-      //Obtenemos los egresos
-      this.finanzasServ.getEgresosByIdService(idUs).subscribe( (resp3:any) =>{
-
-        this.egresos = resp3.egresos || [];
-        this.egresos.forEach( egre =>{
-          this.valEgresos += egre.valor_egre;
-        })
-
-        this.valIngresos -= this.valEgresos;
-        this.totalAcumula = this.finanza[0].base_fina + this.valIngresos;
-
-      }, (err) =>{ console.error(err)})
-
+      //this.valIngresos -= this.valEgresos;
     }, (err) =>{ console.error(err)})
+    
+    setTimeout(() => {
+      this.totalAcumula = this.valIngresos - this.valEgresos;
+      this.totalAcumula < this.finanza[0].base_fina ? this.saldoRojo = 'saldoRojo' : '';
+    }, 2000);
+  }
+
+
+  public getTotalAcumulado = () =>{
+
   }
 
 
